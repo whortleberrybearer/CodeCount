5edc9f7d9f9a20aa5dab195b5e72bb2e970e8285
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2025-01-25 14:29:38 +0000

    feat: Created solution (#1)

---
+++ src/CodeCount/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CodeCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CodeCount!");
+        }
+    }
+}
