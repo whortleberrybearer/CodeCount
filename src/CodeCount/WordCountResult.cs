fa5a7beb982d6e2ce32e9e2a531bc35b5892dce6
Barry Wheeler <wheelerbarry+github@hotmail.com>
2025-01-25 22:03:31 +0000

    Added WordCountResult

---
+++ src/CodeCount/WordCountResult.cs
@@ -0,0 +1,6 @@
+public class WordCountResult
+{
+    public string Word { get; set; }
+
+    public int Count { get; set; }
+}
--- src/CodeCount/WordCounter.cs
+++ src/CodeCount/WordCounter.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-public class WordCount
-{
-    public string Word { get; set; }
-    public int Count { get; set; }
-}
-
 public class WordCounter
 {
-    public IEnumerable<WordCount> GetWordCounts(Stream stream)
+    public IEnumerable<WordCountResult> GetWordCounts(Stream stream)
     {
         if (stream is null)
         {
@@ -20,7 +14,7 @@ public class WordCounter
 
         if (stream.Length == 0)
         {
-            return Enumerable.Empty<WordCount>();
+            return Enumerable.Empty<WordCountResult>();
         }
 
         using (var reader = new StreamReader(stream))
@@ -28,7 +22,7 @@ public class WordCounter
             var text = reader.ReadToEnd();
             var wordCounts = text.Split(new[] { ' ', '\t', '\n', '\r', '.', ',', ';', '!', '?' }, StringSplitOptions.RemoveEmptyEntries)
                                  .GroupBy(word => word.ToLower())
-                                 .Select(group => new WordCount { Word = group.Key, Count = group.Count() })
+                                 .Select(group => new WordCountResult { Word = group.Key, Count = group.Count() })
                                  .ToList();
 
             return wordCounts;
