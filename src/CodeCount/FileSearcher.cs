03636925d16869e3de403a82c40d5d99b6a38695
Barry Wheeler <wheelerbarry+github@hotmail.com>
2025-01-28 13:06:07 +0000

    Refactor

--- src/CodeCount/FileSearcher.cs
+++ src/CodeCount/FileSearcher.cs
@@ -1,21 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-
 public class FileSearcher
 {
     public IEnumerable<string> GetAllFiles(string directoryPath)
     {
-        if (string.IsNullOrEmpty(directoryPath))
-        {
-            throw new ArgumentException("Directory path cannot be null or empty", nameof(directoryPath));
-        }
-
-        if (!Directory.Exists(directoryPath))
-        {
-            throw new DirectoryNotFoundException($"The directory '{directoryPath}' does not exist.");
-        }
-
         return Directory.EnumerateFiles(directoryPath, "*.*", SearchOption.AllDirectories);
     }
 }
