a3dda4a284b5609c3583ebad8cace3eda205d76a
Barry Wheeler <wheelerbarry+github@hotmail.com>
2025-02-07 19:53:14 +0000

    Added tests and functionality

--- src/CodeCount.Tests/CSharpWordCounterTests.cs
+++ src/CodeCount.Tests/CSharpWordCounterTests.cs
@@ -1,56 +1,67 @@
 public class CSharpWordCounterTests
 {
-    // TODO: Ensure count is correct
-    // TODO: Check variables with numbers
-    // TODO: Check things not contained
-    [Fact]
-    public void GetWordCounts_ExcludesKeywords()
+    public class When_counting_words
     {
-        var counter = new CSharpWordCounter();
-        var text = "public class TestClass { private int value; }";
-        var wordCounts = counter.GetWordCounts(text);
+        [Fact]
+        public void Camel_and_pascal_names_should_be_split()
+        {
+            var counter = new CSharpWordCounter();
 
-        wordCounts.ShouldContainKeyAndValue("TestClass", 1);
-        wordCounts.ShouldContainKeyAndValue("value", 1);
-    }
+            var wordCounts = counter.GetWordCounts("public class TestClass { private int camelCaseVariable; }");
 
-    [Fact]
-    public void GetWordCounts_IncludesKeywords_WhenExcludeKeywordsIsFalse()
-    {
-        var counter = new CSharpWordCounter { ExcludeKeywords = false };
-        var text = "public class TestClass { private int value; }";
-        var wordCounts = counter.GetWordCounts(text);
+            wordCounts.Count.ShouldBe(5);
+            wordCounts.ShouldContainKeyAndValue("test", 1);
+            wordCounts.ShouldContainKeyAndValue("class", 1);
+            wordCounts.ShouldContainKeyAndValue("camel", 1);
+            wordCounts.ShouldContainKeyAndValue("case", 1);
+            wordCounts.ShouldContainKeyAndValue("variable", 1);
+        }
 
-        wordCounts.ShouldContainKeyAndValue("public", 1);
-        wordCounts.ShouldContainKeyAndValue("class", 1);
-        wordCounts.ShouldContainKeyAndValue("TestClass", 1);
-        wordCounts.ShouldContainKeyAndValue("private", 1);
-        wordCounts.ShouldContainKeyAndValue("int", 1);
-        wordCounts.ShouldContainKeyAndValue("value", 1);
-    }
+        [Fact]
+        public void Keywords_should_be_excluded()
+        {
+            var counter = new CSharpWordCounter();
 
-    [Fact]
-    public void GetWordCounts_SplitsCamelAndPascalCase()
-    {
-        var counter = new CSharpWordCounter();
-        var text = "public class TestClass { private int camelCaseVariable; }";
-        var wordCounts = counter.GetWordCounts(text);
+            var wordCounts = counter.GetWordCounts("public class TestClass { private int value; }");
 
-        wordCounts.ShouldContainKeyAndValue("Test", 1);
-        wordCounts.ShouldContainKeyAndValue("Class", 1);
-        wordCounts.ShouldContainKeyAndValue("camel", 1);
-        wordCounts.ShouldContainKeyAndValue("Case", 1);
-        wordCounts.ShouldContainKeyAndValue("Variable", 1);
-    }
+            // Although class is a keyword, it should still be counted as it has been split form TestClass,
+            wordCounts.Count.ShouldBe(2);
+            wordCounts.ShouldContainKeyAndValue("test", 1);
+            wordCounts.ShouldContainKeyAndValue("class", 1);
+        }
 
-    [Fact]
-    public void GetWordCounts_DoesNotSplitNames_WhenSplitNamesIsFalse()
-    {
-        var counter = new CSharpWordCounter { SplitNames = false };
-        var text = "public class TestClass { private int camelCaseVariable; }";
-        var wordCounts = counter.GetWordCounts(text);
+        public class When_keywords_are_not_excluded
+        {
+            [Fact]
+            public void Keywords_should_be_included()
+            {
+                var counter = new CSharpWordCounter { ExcludeKeywords = false };
 
-        wordCounts.ShouldContainKeyAndValue("TestClass", 1);
-        wordCounts.ShouldContainKeyAndValue("camelCaseVariable", 1);
+                var wordCounts = counter.GetWordCounts("public class TestClass { private int value; }");
+
+                wordCounts.Count.ShouldBe(6);
+                wordCounts.ShouldContainKeyAndValue("public", 1);
+                wordCounts.ShouldContainKeyAndValue("class", 2);
+                wordCounts.ShouldContainKeyAndValue("test", 1);
+                wordCounts.ShouldContainKeyAndValue("private", 1);
+                wordCounts.ShouldContainKeyAndValue("int", 1);
+                wordCounts.ShouldContainKeyAndValue("value", 1);
+            }
+        }
+
+        public class When_names_are_not_split
+        {
+            [Fact]
+            public void Names_should_not_be_split()
+            {
+                var counter = new CSharpWordCounter { SplitNames = false };
+
+                var wordCounts = counter.GetWordCounts("public class TestClass { private int camelCaseVariable; }");
+
+                wordCounts.Count.ShouldBe(2);
+                wordCounts.ShouldContainKeyAndValue("testclass", 1);
+                wordCounts.ShouldContainKeyAndValue("camelcasevariable", 1);
+            }
+        }
     }
 }
